026903d89947363208e04a88da9ab4d7b6686100
jbaisden <jasonbaisden@gmail.com>
2019-03-13 16:53:02 -0500

    trying to get this to work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
 import { map } from 'rxjs/operators';
 import { Observable } from 'rxjs';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,9 +12,21 @@ import { Observable } from 'rxjs';
 export class AppComponent implements OnInit {
   constructor(private afs: AngularFirestore) { }
   title = 'FirestoreTests';
-  todos$;
+  todos$: Observable<any>;
+  collection: AngularFirestoreCollection<any>;
 
-  async ngOnInit() {
+  ngOnInit() {
+    this.loadTodo2();
+  }
+
+
+  loadTodo2() {
+    this.collection = this.afs.collection('tasks');
+    console.warn(this.collection);
+    this.todos$ = this.collection.valueChanges();
+  }
+
+  loadTodo1() {
     this.todos$ = this.afs
       .collection('tasks')
       .snapshotChanges()
@@ -26,7 +39,6 @@ export class AppComponent implements OnInit {
           });
         })
       );
-    console.log(this.todos$);
   }
 
   // collection$(path, query?) {
@@ -45,3 +57,4 @@ export class AppComponent implements OnInit {
   // }
 
 } //end class
+
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  settings: { timestampsInSnapshots: true },
   production: false,
   firebase: {
     apiKey: "AIzaSyAYCpMk5pAkY7kn9iK85xBEvBuwEWFGOwQ",
