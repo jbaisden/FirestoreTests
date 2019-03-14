00189c8025934ae1f97109dcca345169f30d00c7
jbaisden <jasonbaisden@gmail.com>
2019-03-14 16:00:41 -0500

    google auth working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
 import { map } from 'rxjs/operators';
 import { Observable } from 'rxjs';
+import { AngularFireAuth } from '@angular/fire/auth';
+import * as firebase from 'firebase';
+
 
 @Component({
   selector: 'app-root',
@@ -9,16 +12,44 @@ import { Observable } from 'rxjs';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  constructor(private afs: AngularFirestore) { }
+
+  constructor(private afs: AngularFirestore, public afAuth: AngularFireAuth) { }
+
   title = 'FirestoreTests';
   // todos$: Observable<any>;
   tasks$: Observable<Array<Task>>;
   tasks: Array<any>;
+  user: firebase.User = null;
+
   // collection: AngularFirestoreCollection<any>;
 
   ngOnInit() {
     // this.subscribeGetTasks();
     this.loadTodo1();
+  }
+
+  googleAuth() {
+    return new Promise<any>((resolve, reject) => {
+      let provider = new firebase.auth.GoogleAuthProvider();
+      provider.addScope('profile');
+      provider.addScope('email');
+      this.afAuth.auth
+        .signInWithPopup(provider)
+        .then(res => {
+          this.user = res.user; // res.user as User;
+          console.log(this.user);
+          resolve(res);
+        }, err => {
+          console.error(err);
+          reject(err);
+        }
+        );
+    });
+  }
+
+  logOut() {
+    this.afAuth.auth.signOut();
+    this.user = null;
   }
 
   subscribeGetTasks() {
@@ -74,4 +105,13 @@ interface Task {
   docId: string;
   name: string;
   email: string;
+}
+
+interface User {
+  email: string;
+  phoneNumber: string;
+  photoURL: string;
+  displayName: string;
+  isAnonymous: boolean;
+  uid: string;
 }
